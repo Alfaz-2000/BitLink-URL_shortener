81e1fd6a2d198df81d74dcbe2db99849dc642140
Alfaz-2000 <alfazappu74@gmail.com>
2026-07-08 21:02:02 +0530

    Initial Next.js project

---
+++ app/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main className="bg-purple-100">
+      <section className="grid grid-cols-2 h-[50vh]">
+         <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-3xl font-bold">The best URL Shortener in the Market</p>
+          <p className="text-sm px-56 text-center">We are the most straightforward URL Shortener in World, We know the hasle, they make you login or make watch big AD's. We got your Back!  </p>
+          <div className='flex gap-3'>
+                <Link href="/generate"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold text-white'>Try Now</button></Link>
+                <Link href="/github"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold text-white'>GitHub</button></Link>
+            </div>
+         </div>
+         <div className="flex justify-start relative w-[500px] h-[400px]">
+          <Image className="mix-blend-darken" alt="url-vector-image" src={"/vector.jpg"} fill={true}/>
+         </div>
+      </section>
+    </main>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between items-center h-16 px-3 bg-purple-700 text-white'>
+        <div className="logo font-bold text-lg">BitLinks</div>
+        <ul className='flex justify-center items-center gap-4'>
+            <Link href="/"><li>Home</li></Link>
+            <Link href="/about"><li>About</li></Link>
+            <Link href="/generate"><li>Shorten</li></Link>
+            <Link href="/contact"><li>Contact Us</li></Link>
+            <li className='flex gap-3'>
+                <Link href="/generate"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>Try Now</button></Link>
+                <Link href="/github"><button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold'>GitHub</button></Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
